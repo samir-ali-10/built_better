d95ad0308383bcdb6031f7f4c39bebb493ec90af
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-07-03 18:32:23 +0200

    our services done

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,13 +1,58 @@
 import React from 'react'
+import icon_1 from "../images/icon-1.png"
+import icon_2 from "../images/icon-2.png"
+import icon_3 from "../images/icon-3.png"
+import icon_4 from "../images/icon-4.png"
 
 
 export default function HomePage() {
+
+    let services = [
+        {
+            service: "furniture",
+            img: icon_1
+        },
+        {
+            service: "office",
+            img: icon_2
+        },
+        {
+            service: "home",
+            img: icon_3
+        },
+        {
+            service: "bedroom",
+            img: icon_4
+        }
+    ]
+
     return (
         <>
             <div className='landing_page'>
                 <h1>furniture</h1>
-                <p>there are many variation of passages of lorem ipsum available, but <br /> the majority have suffered alteration in some fo</p>
+                <p>There are many variation of passages of lorem ipsum available, but <br /> the majority have suffered alteration in some fo</p>
                 <button>contact us</button>
+            </div>
+
+            <div className="services">
+                <div className="container">
+                    <div className="heading">
+                        <h2>our services</h2>
+                        <p>There are many variations of passages of lorem ipsum</p>
+                    </div>
+                    <div className="our_services">
+                        {
+                            services.map((ser, index) =>
+                                <div className='service' key={index}>
+                                    <img src={ser.img} alt="icon" />
+                                    <h3>{ser.service}</h3>
+                                    <p>There are many variation of passages of lorem ipsum available, but the</p>
+                                    <button>read more</button>
+                                </div>
+                            )
+                        }
+                    </div>
+                </div>
             </div>
         </>
     )
