bc5b2a23e48da907a10b620bd4611e009789f0c6
Samir Ali <82404683+samir142@users.noreply.github.com>
2023-07-05 13:56:53 +0200

    Half Home Page Done

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -4,6 +4,8 @@ import icon_2 from "../images/icon-2.png"
 import icon_3 from "../images/icon-3.png"
 import icon_4 from "../images/icon-4.png"
 import aboutImage from "../images/img-1.png"
+import furnitureOne from "../images/img-2.png"
+import furnitureTwo from "../images/img-3.png"
 
 
 export default function HomePage() {
@@ -63,7 +65,43 @@ export default function HomePage() {
                         <p>There are many variations of passages of lorem ipsum, There are many variations of passages of lorem ipsum, There are many variations of passages of lorem ipsum, There are many variations of passages of lorem ipsum</p>
                         <button>read more</button>
                     </div>
-                        <img src={aboutImage} alt="aboutImage" />
+                    <img src={aboutImage} alt="aboutImage" />
+                </div>
+            </div>
+
+            <div className="furniture">
+                <div className="container">
+                    <div className="heading">
+                        <h2>our furniture</h2>
+                        <p>There are many variation of passages of lorem ipsum</p>
+                    </div>
+                    <div className="categories">
+                        <div className="kitchen">
+                            <div className="image">
+                                <img src={furnitureOne} alt="furnitureOne" />
+                            </div>
+                            <h3>tempor incididunt ut labore et dolore</h3>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, eligendi. Et perspiciatis ipsam accusamus, quidem qui aliquid tenetur placeat doloribus suscipit explicabo error ut nostrum voluptate iste dolores omnis ad.</p>
+                        </div>
+                        <div className="sink">
+                            <div className="image">
+                                <img src={furnitureTwo} alt="furnitureTwo" />
+                            </div>
+                            <h3>tempor incididunt ut labore et dolore</h3>
+                            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae, eligendi. Et perspiciatis ipsam accusamus, quidem qui aliquid tenetur placeat doloribus suscipit explicabo error ut nostrum voluptate iste dolores omnis ad.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="who_we_are">
+                <div className="container">
+                    <div className="heading">
+                        <h2>who we are ?</h2>
+                        <p>designers & innovators</p>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error placeat modi culpa officiis pariatur alias numquam saepe deserunt fuga, reiciendis aut ex. Nesciunt tempore a mollitia assumenda voluptatem porro doloribus?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt quos assumenda impedit hic omnis, dolor, consequatur totam dolorum, fugiat delectus facere aperiam quo qui itaque harum! Nemo est laboriosam eos?</p>
+                    <button>get a quote</button>
                 </div>
             </div>
         </>
